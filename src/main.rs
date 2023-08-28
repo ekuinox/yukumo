fde8b7b39c86632af06a27cfb42da13b8ceaa988
ekuinox <depkey@me.com>
2023-08-28 23:57:47 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+mod notion;
+
+use anyhow::Result;
+use clap::Parser;
+use dotenv::dotenv;
+
+use crate::notion::Notion;
+
+#[derive(Parser, Debug)]
+pub struct Cli {
+    #[clap(short, long)]
+    pub page_id: String,
+
+    #[clap(short, long, env = "NOTION_TOKEN_V2")]
+    pub token_v2: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let _ = dotenv();
+
+    let cli = Cli::parse();
+
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "info");
+    }
+    env_logger::init();
+    
+    let client = Notion::new(cli.token_v2);
+    let data = client.get_page_data(&cli.page_id).await?;
+
+    println!("{data:#?}");
+    Ok(())
+}
---
+++ src/notion.rs
@@ -0,0 +1,84 @@
+use anyhow::{ensure, Context, Result};
+use reqwest::header;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug)]
+pub struct Notion {
+    token_v2: String,
+}
+
+impl Notion {
+    pub fn new(token_v2: String) -> Notion {
+        Notion { token_v2 }
+    }
+
+    pub async fn get_page_data(&self, page_id: &str) -> Result<PageDataResponse> {
+        let page_id = to_dashed_id(page_id).context("convert to dashed id")?;
+        let client = reqwest::Client::builder().build()?;
+        let req = PageDataRequest {
+            r#type: "block-space".to_string(),
+            block_id: page_id,
+            name: "page".to_string(),
+            save_parent: false,
+            show_move_to: false,
+        };
+        let res = client
+            .post("https://www.notion.so/api/v3/getPublicPageData")
+            .header(header::CONTENT_TYPE, "application/json")
+            .header(header::COOKIE, format!("token_v2={}", self.token_v2))
+            .json(&req)
+            .send()
+            .await
+            .context("request")?;
+        ensure!(res.status().is_success(), res.status());
+        let res = res.json::<PageDataResponse>().await.context("parse json")?;
+        Ok(res)
+    }
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Eq, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PageDataRequest {
+    pub r#type: String,
+    pub block_id: String,
+    pub name: String,
+    pub save_parent: bool,
+    pub show_move_to: bool,
+}
+
+#[derive(Deserialize, Serialize, PartialEq, Eq, Clone, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PageDataResponse {
+    pub beta_enabled: bool,
+    pub can_join_space: bool,
+    pub owner_user_id: String,
+    pub page_id: String,
+    pub public_access_role: String,
+    pub require_login: bool,
+    pub space_domain: String,
+    pub space_id: String,
+    pub space_name: String,
+    pub user_has_explicit_access: bool,
+}
+
+/// id をダッシュでつなげたやつにする
+pub fn to_dashed_id(id: &str) -> Result<String> {
+    let id = id.replace('-', "");
+    ensure!(id.len() == 32);
+
+    let a = &id[0..8];
+    let b = &id[8..12];
+    let c = &id[12..16];
+    let d = &id[16..20];
+    let e = &id[20..];
+    Ok(format!("{a}-{b}-{c}-{d}-{e}"))
+}
+
+#[test]
+fn test_to_dashed_id() {
+    const ID: &str = "2131b10cebf64938a1277089ff02dbe4";
+    assert_eq!(
+        to_dashed_id(ID).ok(),
+        Some("2131b10c-ebf6-4938-a127-7089ff02dbe4".to_string())
+    );
+}
